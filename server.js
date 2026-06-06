4c05f994456294542098f9f0f597a48f4a35fb5f
Shekhar Ramamurthy <shekhar.ramamurthy@yahoo.com>
2026-06-06 04:35:22 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8089;
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log('Error while starting server...');
+    } else {
+        console.log(`App Started on port ${port}`);
+    }
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello from OpenShift, I am version 1.0');
+});
